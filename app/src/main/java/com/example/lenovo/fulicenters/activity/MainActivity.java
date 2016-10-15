fc992ba1dcb4b0a4fd0f62ee6136659f4e326398
刘宁 <1039212799@qq.com>
2016-10-15 19:20:06 +0800

    修改项目图标、添加闪屏

---
+++ app/src/main/java/com/example/lenovo/fulicenters/activity/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.lenovo.fulicenters.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.lenovo.fulicenters.R;
+
+public class MainActivity extends AppCompatActivity {
+    //private  final long sleeptime=2000;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+   /* @Override
+    protected void onStart() {
+        super.onStart();
+
+    }*/
+}
---
+++ app/src/main/java/com/example/lenovo/fulicenters/activity/SplashActivity.java
@@ -0,0 +1,40 @@
+package com.example.lenovo.fulicenters.activity;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.lenovo.fulicenters.R;
+
+public class SplashActivity extends AppCompatActivity {
+    private final long sleepTime = 2000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                long start = System.currentTimeMillis();
+                //Creat db
+                long costTime = System.currentTimeMillis() - start;
+                if (sleepTime - costTime > 0) {
+                    try {
+                        Thread.sleep(sleepTime - costTime);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                    startActivity(intent);
+                    finish();
+                }
+            }
+        }).start();
+    }
+}
