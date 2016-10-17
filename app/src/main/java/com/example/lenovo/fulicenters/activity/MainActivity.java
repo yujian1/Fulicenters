1fa3a349ba85628c8e2b8e698f2030b222abaf8a
刘宁 <1039212799@qq.com>
2016-10-17 11:30:23 +0800

    添加服务器接口文件

--- app/src/main/java/com/example/lenovo/fulicenters/activity/MainActivity.java
+++ app/src/main/java/com/example/lenovo/fulicenters/activity/MainActivity.java
@@ -1,31 +1,42 @@
 package com.example.lenovo.fulicenters.activity;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.RadioButton;
+import android.widget.TextView;
 
 import com.example.lenovo.fulicenters.R;
 import com.example.lenovo.fulicenters.utils.L;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.newGoods)
+    RadioButton newGoods;
+    @BindView(R.id.boutique)
+    RadioButton boutique;
+    @BindView(R.id.category)
+    RadioButton category;
+    @BindView(R.id.layout_cart)
+    RadioButton layoutCart;
+    @BindView(R.id.tvCart)
+    TextView tvCart;
+    @BindView(R.id.personal)
+    RadioButton personal;
+    
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
         L.i("MainActivity onCreate");
     }
 
 
-    public void onnewgoods(View view) {
-    }
-
-    public void onPersonal(View view) {
-    }
-
-    public void oncategory(View view) {
-    }
-
-    public void onboutique(View view) {
+    public void onClick(View view) {
     }
 }
