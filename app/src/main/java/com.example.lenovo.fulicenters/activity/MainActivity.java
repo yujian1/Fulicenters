ab430a3bb9a70c9d8875afa372693613b33a9572
刘宁 <1039212799@qq.com>
2016-10-20 16:43:49 +0800

    显示分类首页

--- app/src/main/java/com.example.lenovo.fulicenters/activity/MainActivity.java
+++ app/src/main/java/com.example.lenovo.fulicenters/activity/MainActivity.java
@@ -9,6 +9,7 @@ import android.widget.TextView;
 
 import com.example.lenovo.fulicenters.R;
 import com.example.lenovo.fulicenters.fragment.BoutiqueFragment;
+import com.example.lenovo.fulicenters.fragment.CategoryFragment;
 import com.example.lenovo.fulicenters.fragment.NewGoodsFragment;
 import com.example.lenovo.fulicenters.utils.L;
 
@@ -36,6 +37,7 @@ public class MainActivity extends BaseActivity  {
     Fragment[]mFragments;
     NewGoodsFragment mNewGoodsFragment;
     BoutiqueFragment mBoutiqueFragment;
+    CategoryFragment mCategoryFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,14 +51,17 @@ public class MainActivity extends BaseActivity  {
         mFragments=new Fragment[5];
         mNewGoodsFragment=new NewGoodsFragment();
         mBoutiqueFragment=new BoutiqueFragment();
+        mCategoryFragment=new CategoryFragment();
         mFragments[0]=mNewGoodsFragment;
         mFragments[1]=mBoutiqueFragment;
+        mFragments[2]=mCategoryFragment;
         getSupportFragmentManager()
                 .beginTransaction()
                 .add(R.id.fragment_container,mNewGoodsFragment)
                 .add(R.id.fragment_container,mBoutiqueFragment)
-
+                .add(R.id.fragment_container,mCategoryFragment)
                 .hide(mBoutiqueFragment)
+                .hide(mCategoryFragment)
                 .show(mNewGoodsFragment)
                 .commit();
     }
